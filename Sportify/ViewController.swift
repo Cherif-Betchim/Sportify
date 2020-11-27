8258ab679d9ccea253f6ccc52759e24052792912
Cherif <cherif-betchim@hotmail.com>
2020-11-27 08:54:26 +0100

    Initial Commit

---
+++ Sportify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sportify
+//
+//  Created by etudiant on 27/11/2020.
+//  Copyright © 2020 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
